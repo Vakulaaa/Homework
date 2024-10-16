adb5e3775faacb828d6b3c0dc9f85e2d99d510bd
Yuniia <yuniiaolkhova@gmail.com>
2024-10-16 22:15:11 +0300

    change: calculator was added

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# Basic terminal calculator
+
 number1 = int(input("Write the first value: "))
 number2 = int(input("Write the second value: "))
 operation = input("Select an operation: ")
@@ -9,6 +9,9 @@ elif operation == "-":
 elif operation == "*":
     print(number1 * number2)
 elif operation == "/":
-    print(number1 / number2)
+    if number2 == 0:
+        print("Second value can't be 0")
+    else:
+        print(number1 / number2)
 else:
     print("Invalid operation, try again.")
