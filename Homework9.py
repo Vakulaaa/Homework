72757b7a3eaa6fbd40aa96b7231be02b2f096163
Yuniia <yuniiaolkhova@gmail.com>
2024-10-24 21:38:10 +0300

    change: Homework8 was added

---
+++ Homework9.py
@@ -0,0 +1,24 @@
+
+USER_SOLUTION = "yes"
+
+while True:
+    number1 = int(input("Write the first value: "))
+    number2 = int(input("Write the second value: "))
+    operation = input("Select operation: ")
+    if operation == "+":
+       print(number1 + number2)
+    elif operation == "-":
+        print(number1 - number2)
+    elif operation == "*":
+        print(number1 * number2)
+    elif operation == "/":
+        if number2 == 0:
+            print("Second value can't be 0")
+        else:
+            print(number1 / number2)
+    else:
+        print("Invalid operation, try again.")
+    solution = input("Do you want to continue? ")
+    if solution != USER_SOLUTION:
+        print("Exiting from program...")
+        break
