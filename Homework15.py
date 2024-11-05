74db0cafa790bbd6a0b2f1f211b27cf126be2b81
Yuniia <yuniiaolkhova@gmail.com>
2024-11-05 18:43:24 +0200

    change: Homework10 was added

---
+++ Homework15.py
@@ -0,0 +1,10 @@
+
+import string
+
+
+def my_hashtag(my_string):
+    new_string = "".join(char for char in my_string if char not in string.punctuation).split()
+    hashtag = "#" + "".join(word.capitalize() for word in new_string)
+    print(hashtag[:140])
+
+my_hashtag("my little pony")
