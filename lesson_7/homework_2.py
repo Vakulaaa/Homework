c405aa622a7a5cc9e7f87b6b5dec8bd396dd6757
Yuniia <yuniiaolkhova@gmail.com>
2024-11-07 20:26:10 +0200

    change: Homework10 was added

--- lesson_7/homework_2.py
+++ lesson_7/homework_2.py
@@ -3,6 +3,6 @@ def correct_sentence(text):
     text = text.replace(",", ".")
     if not text.endswith("."):
         text+= "."
-    print(text.title())
+    return text.title()
 
 correct_sentence("greetings, friends")
