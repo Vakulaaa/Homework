22fb59e0c1e91e1a70ddf3e027b9867712346945
Yuniia <yuniiaolkhova@gmail.com>
2024-11-08 21:30:26 +0200

    change: Homework10 was added

---
+++ lesson_8/homework_8.3.py
@@ -0,0 +1,11 @@
+
+def find_unique_value(numbers):
+    counts = {}
+    for num in numbers:
+        counts[num] = counts.get(num, 0) + 1
+
+    for num, count in counts.items():
+        if count == 1:
+            return num
+
+find_unique_value()
